3898534e9484adeb24293ec05e3051324e6e5026
Julian Grijalba <caprijuda@gmail.com>
2017-08-27 02:24:10 -0500

    Reestructured operators and comma logic

--- Taller1/src/main/webapp/calc.js
+++ Taller1/src/main/webapp/calc.js
@@ -1,14 +1,32 @@
-function imprimir(n) {
-    var pantalla = document.getElementById('pantalla');
-    var actual = pantalla.textContent;
-    if (actual == 0) {
-        pantalla.innerHTML = n;
-    } else if (actual.length <= 15) {
-        pantalla.innerHTML = actual.toString() + n.toString();
+var pantalla;
+window.onload = function () {
+   pantalla = document.getElementById('pantalla');
+}
+
+function writeNumber(n){
+    pantalla.innerHTML=pantalla.textContent==0?n:pantalla.textContent+n
+};
+function writeOperator(n){
+    var text = pantalla.textContent;
+    pantalla.innerHTML=$.isNumeric(text.slice(-1))?text+n:text;
+};
+
+function comma(){
+    var text = pantalla.textContent;
+    var numbers = text.split(/[+*\-/]/);
+    if (!numbers[numbers.length-1].includes('.') && numbers[numbers.length-1]!="") {
+        pantalla.innerHTML=text+'.';
     }
 };
 
+function BorrarTodo(){
+    pantalla.innerHTML="0";
+};
+function Borrar(){
+    var text = pantalla.textContent;
+    pantalla.innerHTML=text.length>1?text.slice(0,-1):'0';
+};
+
 function solve(){
-    var pantalla = document.getElementById('pantalla');
     pantalla.innerHTML = eval(pantalla.textContent.replace(/x/g , "*"));
-}
+};
