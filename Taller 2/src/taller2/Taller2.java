8b4d92da9b026956fb0834202b854b3fdd4956cb
Julian Grijalba <caprijuda@gmail.com>
2017-09-06 17:47:27 -0500

    Revert "Revert "Taller 2 versión 1.0""
    This reverts commit 591ca128cbb529f3149f8ac636b1cb72220a5326.

---
+++ Taller 2/src/taller2/Taller2.java
@@ -0,0 +1,13 @@
+package taller2;
+
+public class Taller2 {
+
+	public static void main(String[] args) {
+		Ascensor ascensor = Instancia.newAscensor();
+		ascensor.subir2Pisos();
+		ascensor.subir1Piso();
+		ascensor.bajar3Pisos();
+		ascensor.subir1Piso();
+		ascensor.bajar1Piso();	
+	}
+}
