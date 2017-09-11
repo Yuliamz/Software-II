ffe8a5de29ebffc16c6c82641cdbb160c6d9c500
ViejoHarry <andresuptc@outlook.com>
2017-09-11 15:51:03 -0500

    added estudiante, profesor, materia.

--- Taller4/web/js/Modelo.js
+++ Taller4/web/js/Modelo.js
@@ -15,3 +15,19 @@ var municipios=[
     {"id":2, "nombre":"Duitama"},
     {"id":3, "nombre":"Sogamoso"},
 ];
+
+var idEstudiante=1;
+var estudiantes=[
+    {"id":1,"nombre":"Carlos","apellido":"Arias","codigo":"201312244","documento":"1052","fechaNacimineto" :new Date(99, 5, 24)},
+];
+
+var idProfesor=1;
+var profesores=[
+    {"id":1,"nombre":"Carlos","apellido":"Arias","documento":"1052"},
+];
+
+var idMaterias=1;
+var materias=[
+        {"id":1,"nombre":"Carlos","creditos":parseInt("3")},
+
+];
