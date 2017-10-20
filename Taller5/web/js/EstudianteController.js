28f11cb34bdadf5de34c1bb5a042dedd331bb35d
Julian David <caprijuda@gmail.com>
2017-10-20 15:16:27 -0500

    Busquedas por nombre y apellido agregadas en nueva pestaña

--- Taller5/web/js/EstudianteController.js
+++ Taller5/web/js/EstudianteController.js
@@ -3,8 +3,43 @@
 module.controller('EstudianteCtrl', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
         //listar
         $scope.lista = [];
+        $scope.listaFiltrada = [];
         $scope.datosFormulario = {};
-        $scope.panelEditar = false;
+        $scope.opcionBuscar = "Nombre";
+        $scope.panelCrear = true;
+        $scope.query="";
+        $scope.textApellido=true;
+        $scope.textNombre=false;
+        
+        $scope.buscarPorNombre = function (){
+            $scope.query="./webresources/Estudiante/buscar?nombre="+$scope.nombres;
+            $http.get($scope.query, {})
+                    .success(function (data, status, headers, config) {
+                        $scope.listaFiltrada = data;
+                    }).error(function (data, status, headers, config) {
+                alert('Error al consultar la informaci\xf3n, por favor intente m\xe1s tarde');
+            });
+        };
+        $scope.buscarPorApellido = function (){
+            $scope.query="./webresources/Estudiante/buscar?apellido="+$scope.apellidos;
+            $http.get($scope.query, {})
+                    .success(function (data, status, headers, config) {
+                        $scope.listaFiltrada = data;
+                    }).error(function (data, status, headers, config) {
+                alert('Error al consultar la informaci\xf3n, por favor intente m\xe1s tarde');
+            });
+        };
+        $scope.buscarPorNombreYApellido = function (){
+            $scope.query="./webresources/Estudiante/buscar?nombre="+$scope.nombres+"&apellido="+$scope.apellidos;
+            $http.get($scope.query, {})
+                    .success(function (data, status, headers, config) {
+                        $scope.listaFiltrada = data;
+                    }).error(function (data, status, headers, config) {
+                alert('Error al consultar la informaci\xf3n, por favor intente m\xe1s tarde');
+            });
+        };
+        
+        
         $scope.listar = function () {
             $http.get('./webresources/Estudiante', {})
                     .success(function (data, status, headers, config) {
@@ -37,8 +72,44 @@ module.controller('EstudianteCtrl', ['$scope', '$filter', '$http', function ($sc
         $scope.listar();
         //guardar
         $scope.nuevo = function () {
-            $scope.panelEditar = true;
+            console.log("Abre el panel para crear un  nuevo estudiante");
+            $scope.panelCrear = true;
+            $scope.panelListar = false;
+            $scope.panelBuscar = false;
+            
             $scope.datosFormulario = {};
+        };
+        
+        $scope.buscar = function () {
+            console.log("Abre el panel para buscar los estudiantes");
+            $scope.panelCrear = false;
+            $scope.panelListar = false;
+            $scope.panelBuscar = true;
+            $scope.datosFormulario = {};
+        };
+        
+        $scope.mostrar = function () {
+            console.log("Abre el panel para listar los estudiantes");
+            $scope.panelCrear = false;
+            $scope.panelListar = true;
+            $scope.panelBuscar = false;
+            $scope.datosFormulario = {};
+        };
+        
+        $scope.cambio = function(){
+            $scope.textApellido=$scope.opcionBuscar!=="Nombre"?false:true;
+            $scope.textNombre=$scope.opcionBuscar=="Apellido"?true:false;
+            console.log("Se cambio a: "+$scope.opcionBuscar);
+        };
+        $scope.busquedaFiltrada = function(){
+            console.log("Se busca por: "+$scope.opcionBuscar);
+            switch ($scope.opcionBuscar){
+                case "Nombre": $scope.buscarPorNombre(); break;
+                case "Apellido": $scope.buscarPorApellido(); break;
+                case "Nombre y Apellido": $scope.buscarPorNombreYApellido(); break;
+                    default :$scope.buscarPorNombre(); break;
+            }
+            
         };
 
         $scope.guardar = function () {
@@ -50,20 +121,21 @@ module.controller('EstudianteCtrl', ['$scope', '$filter', '$http', function ($sc
             $http.post('./webresources/Estudiante', JSON.stringify($scope.datosFormulario), {}
             ).success(function (data, status, headers, config) {
                 alert("Los datos han sido guardados con Exito");
-                $scope.panelEditar = false;
+            $scope.panelCrear = false;
+            $scope.panelListar = true;
+            $scope.panelBuscar = false;
                 $scope.listar();
             }).error(function (data, status, headers, config) {
                 alert('Error al guardar la informaci\xf3n, por favor intente m\xe1s tarde');
             });
         };
-        $scope.cancelar = function () {
-            $scope.panelEditar = false;
-            $scope.datosFormulario = {};
-        };
+
 
         //editar
         $scope.editar = function (data) {
-            $scope.panelEditar = true;
+            $scope.panelCrear = true;
+            $scope.panelListar = false;
+            $scope.panelBuscar = false;
             $scope.datosFormulario = data;
         };
         //eliminar
